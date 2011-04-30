fdbf38da47d43240d50e856ed09cc310fefa05da
imolenaar <imolenaar28@gmail.com>
2011-04-30 22:03:15 +0000

    -a

---
+++ MITProjects/mitassign00.py
@@ -0,0 +1,6 @@
+# Basic program which asks and prints out user's name
+
+firstName = raw_input("Enter your first name: ")
+lastName = raw_input("Enter your last name: ")
+
+print lastName + " " + firstName
